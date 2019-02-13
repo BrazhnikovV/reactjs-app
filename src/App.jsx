0fbaf48a04490bbce36932a9d0a6cb6894d03b71
BrazhnikovV <brazhnikov_ve@mail.ru>
2019-02-13 22:04:28 +0300

    Подключил bootstrap-4.3.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import ButtonSpin from './ButtonSpin';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-          <div className="slots">
-              <ButtonSpin/>
-          </div>
       </div>
     );
   }
